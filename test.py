359260a71d8d0a4d23caacbe680f8bc3737a3364
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-11-25 10:14:36 -0300

    tentando traducao

--- test.py
+++ test.py
@@ -2,18 +2,24 @@ import tweepy
 import re
 import csv
 from textblob import TextBlob
+# from translate import Translator
 from googletrans import Translator
 
 def sentiment_analiser(tweet):
-    translator = Translator()
+    translator = Translator(to_lang="en")
     print(tweet)
-    tweet = translator.translate(tweet, dest='en')
-    print(tweet.text)
-    tweet = TextBlob(tweet.text)
+    en_tweet = translator.translate(tweet)
+    # print(tweet.text)
+    # en_tweet = TextBlob(en_tweet.text)
 
-    if tweet.polarity > 0:
+    translator= Translator(from_lang="pt", to_lang="en")
+    en_tweet = translator.translate(tweet)
+    en_tweet = TextBlob(en_tweet)
+
+
+    if en_tweet.polarity > 0:
         sentimento = '1'
-    elif tweet.polarity < 0:
+    elif en_tweet.polarity < 0:
         sentimento = '0'
     else:
         sentimento = '0.5'
@@ -23,19 +29,22 @@ def sentiment_analiser(tweet):
 
 tweets = []
 print('lendo arquivo e fazendo preprocessamento\n')
-with open('bolsonaro_colocar_sentimento.csv', encoding='utf-8') as csvfile:
+with open('bolsonaro_colocar_sentimento2.csv', encoding='utf-8') as csvfile:
     readCSV = csv.reader(csvfile)
+    i = 0
     for row in readCSV:
-        try:
-            tweet_com_sentimento = sentiment_analiser(row[0])
-        except Exception as e:
-            print(str(e))
-            continue
-        tweets.append(tweet_com_sentimento)
+        aux = ' '.join(row)
+        translator = Translator()
+        en_tweet = translator.translate(aux, dest='en')
+        # tweet_com_sentimento = sentiment_analiser(aux)
+        tweets.append(aux)
+        print(i)
+        i = i + 1
 print('preprocessamento terminado')
 
+
 print('escrevendo treino\n')
-with open('bolsonaro_com_sentimento.csv', mode='w', encoding='utf-8', newline='') as csvfile:
+with open('bolsonaro_colocar_sentimento3.csv', mode='w', encoding='utf-8', newline='') as csvfile:
     writeCSV = csv.writer(csvfile)
     i = 0
     while (i < len(tweets)):
@@ -44,6 +53,11 @@ with open('bolsonaro_com_sentimento.csv', mode='w', encoding='utf-8', newline=''
 print('treino escrito\n')
 
 
+# tweet = 'Eu te amo'
+# translator = Translator()
+# print(tweet)
+# en_tweet = translator.translate(tweet, dest='en')
+# print(en_tweet.text)
 
 # import csv
 # import nltk #Natural Language Processing
