9466ab65c7a6e970cd645ece07d9c2fef45146d9
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-11-28 13:44:44 -0300

    testando outra abordagem

--- test2.py
+++ test2.py
@@ -1,119 +1,54 @@
-import time
-import tweepy
-import re
-import csv
+from textblob.classifiers import NaiveBayesClassifier
 from textblob import TextBlob
-from translate import Translator
-#from googletrans import Translator
+import csv
 
-def sentiment_analiser(tweet):
-    #translator = Translator(to_lang="en")
-    #print(tweet)
-    #en_tweet = translator.translate(tweet)
-    # print(tweet.text)
-    # en_tweet = TextBlob(en_tweet.text)
+def tratamento_dos_dados(dados):
+    dados_tratados = []
+    for dado in dados:
+        dado = dado.split('    ')
+        dados_tratados.append(dado)
 
-    translator= Translator(from_lang="pt", to_lang="en")
-    en_tweet = translator.translate(tweet)
-    en_tweet = TextBlob(en_tweet)
+    return dados_tratados
 
+def obter_dados_das_fontes():
+    dados = []
+    print('lendo arquivo e fazendo preprocessamento\n')
+    with open('bolsonaro_colocar_sentimento.csv', encoding='utf-8') as csvfile:
+        readCSV = csv.reader(csvfile)
+        for row in readCSV:
+            dados.append(row[0])
+    print('preprocessamento terminado')
 
-    if en_tweet.polarity > 0:
-        sentimento = '1'
-    elif en_tweet.polarity < 0:
-        sentimento = '0'
-    else:
-        sentimento = '0.5'
+    return dados
 
-    return tweet + '\t' + sentimento
-    
+def pre_processamento():
+    dados = obter_dados_das_fontes()
+    dados_tratados = tratamento_dos_dados(dados)
 
-tweets = []
-print('lendo arquivo e fazendo preprocessamento\n')
-with open('bolsonaro_colocar_sentimento3.csv', encoding='utf-8') as csvfile:
-    readCSV = csv.reader(csvfile)
-    i = 0
-    for row in readCSV:
-        if i % 400 == 0 and i != 0:
-            time.sleep(30)
-        aux = ' '.join(row)
-        #translator = Translator()
-        #en_tweet = translator.translate(aux, dest='en')
-        tweet_com_sentimento = sentiment_analiser(aux)
-        tweets.append(tweet_com_sentimento)
-        print(i)
-        i = i + 1
-print('preprocessamento terminado')
+    return dividir_dados_para_treino_e_validacao(dados_tratados)
 
+def dividir_dados_para_treino_e_validacao(dados):
+    quantidade_total = len(dados)
+    percentual_para_treino = 0.75
+    treino = []
+    validacao = []
 
-print('escrevendo treino\n')
-with open('bolsonaro_colocar_sentimento31.csv', mode='w', encoding='utf-8', newline='') as csvfile:
-    writeCSV = csv.writer(csvfile)
-    i = 0
-    while (i < len(tweets)):
-        writeCSV.writerow([tweets[i]])
-        i = i + 1
-print('treino escrito\n')
+    for indice in range(0, quantidade_total):
+        if indice < quantidade_total * percentual_para_treino:
+            treino.append(dados[indice])
+        else:
+            validacao.append(dados[indice])
 
+    return treino, validacao
 
-# tweet = 'Eu te amo'
-# translator = Translator()
-# print(tweet)
-# en_tweet = translator.translate(tweet, dest='en')
-# print(en_tweet.text)
+train_set, test_set = pre_processamento()
 
-# import csv
-# import nltk #Natural Language Processing
-# import re #Python's regular expression
-# from nltk.tokenize import word_tokenize 
-# from string import punctuation 
-# from nltk.corpus import stopwords
-# from more_itertools import unique_everseen
-# import emoji        
+cl = NaiveBayesClassifier(train_set)
+accuracy = cl.accuracy(test_set)
 
+frase = 'Bolsonaro eu te amo'
 
+blob = TextBlob(frase,classifier=cl)
 
-# tweets = []
-# all_words = []
-# tweet = []
-# i = 0
-# print('lendo\n')
-# with open('bolsonaro_treino2.csv', mode='r', encoding='utf-8') as csvfile:
-#     readCSV = csv.reader(csvfile)
-#     for row in readCSV:
-#         tweets.append(row[0])
-#         tweet = word_tokenize(row[0])
-#         for word in tweet:
-#             all_words.append(word)
-
-# def extract_features(tweet):
-#     tweet_words=set(tweet)
-#     features={}
-#     for word in word_features:
-#         features['contains(%s)' % word]=(word in tweet_words)
-#     return features 
-
-# print('montando palavras')
-# wordlist = nltk.FreqDist(all_words)
-# word_features = wordlist.keys()
-
-# print('treinando')
-# trainingFeatures=nltk.classify.apply_features(extract_features,tweets)
-
-# print(trainingFeatures)
-
-# NBayesClassifier=nltk.NaiveBayesClassifier.train(trainingFeatures)
-
-# print('label')
-
-# NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0])) for tweet in tweets]
-
-
-# print('analisando resultado')
-# if NBResultLabels.count('positive') > NBResultLabels.count('negative'):
-#     print("Overall Positive Sentiment")
-#     print("Positive Sentiment Percentage = " + str(100*NBResultLabels.count('positive')/len(NBResultLabels)) + "%")
-# else: 
-#     print("Overall Negative Sentiment")
-#     print("Negative Sentiment Percentage = " + str(100*NBResultLabels.count('negative')/len(NBResultLabels)) + "%")
-
+print('Esta frase é de caráter:{}'.format(blob.classify()))
+print('Precisão da previsão:{}'.format(accuracy))
--- test3.py
+++ test3.py
@@ -1,119 +1,19 @@
-import time
-import tweepy
+import nltk
 import re
-import csv
-from textblob import TextBlob
-from translate import Translator
-#from googletrans import Translator
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn.model_selection import cross_val_predict
 
-def sentiment_analiser(tweet):
-    #translator = Translator(to_lang="en")
-    #print(tweet)
-    #en_tweet = translator.translate(tweet)
-    # print(tweet.text)
-    # en_tweet = TextBlob(en_tweet.text)
-
-    translator= Translator(from_lang="pt", to_lang="en")
-    en_tweet = translator.translate(tweet)
-    en_tweet = TextBlob(en_tweet)
-
-
-    if en_tweet.polarity > 0:
-        sentimento = '1'
-    elif en_tweet.polarity < 0:
-        sentimento = '0'
-    else:
-        sentimento = '0.5'
-
-    return tweet + '\t' + sentimento
-    
-
+dataset = pd.read_csv('bolsonaro_colocar_sentimento3.csv')
 tweets = []
-print('lendo arquivo e fazendo preprocessamento\n')
-with open('bolsonaro_colocar_sentimento4.csv', encoding='utf-8') as csvfile:
-    readCSV = csv.reader(csvfile)
-    i = 0
-    for row in readCSV:
-        if i % 400 == 0 and i != 0:
-            time.sleep(30)
-        aux = ' '.join(row)
-        #translator = Translator()
-        #en_tweet = translator.translate(aux, dest='en')
-        tweet_com_sentimento = sentiment_analiser(aux)
-        tweets.append(tweet_com_sentimento)
-        print(i)
-        i = i + 1
-print('preprocessamento terminado')
+classes = []
+for text,sentiment in dataset.values:
+    tweets.append[text]
+    classes.append[sentiment]
 
-
-print('escrevendo treino\n')
-with open('bolsonaro_colocar_sentimento41.csv', mode='w', encoding='utf-8', newline='') as csvfile:
-    writeCSV = csv.writer(csvfile)
-    i = 0
-    while (i < len(tweets)):
-        writeCSV.writerow([tweets[i]])
-        i = i + 1
-print('treino escrito\n')
-
-
-# tweet = 'Eu te amo'
-# translator = Translator()
-# print(tweet)
-# en_tweet = translator.translate(tweet, dest='en')
-# print(en_tweet.text)
-
-# import csv
-# import nltk #Natural Language Processing
-# import re #Python's regular expression
-# from nltk.tokenize import word_tokenize 
-# from string import punctuation 
-# from nltk.corpus import stopwords
-# from more_itertools import unique_everseen
-# import emoji        
-
-
-
-# tweets = []
-# all_words = []
-# tweet = []
-# i = 0
-# print('lendo\n')
-# with open('bolsonaro_treino2.csv', mode='r', encoding='utf-8') as csvfile:
-#     readCSV = csv.reader(csvfile)
-#     for row in readCSV:
-#         tweets.append(row[0])
-#         tweet = word_tokenize(row[0])
-#         for word in tweet:
-#             all_words.append(word)
-
-# def extract_features(tweet):
-#     tweet_words=set(tweet)
-#     features={}
-#     for word in word_features:
-#         features['contains(%s)' % word]=(word in tweet_words)
-#     return features 
-
-# print('montando palavras')
-# wordlist = nltk.FreqDist(all_words)
-# word_features = wordlist.keys()
-
-# print('treinando')
-# trainingFeatures=nltk.classify.apply_features(extract_features,tweets)
-
-# print(trainingFeatures)
-
-# NBayesClassifier=nltk.NaiveBayesClassifier.train(trainingFeatures)
-
-# print('label')
-
-# NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0])) for tweet in tweets]
-
-
-# print('analisando resultado')
-# if NBResultLabels.count('positive') > NBResultLabels.count('negative'):
-#     print("Overall Positive Sentiment")
-#     print("Positive Sentiment Percentage = " + str(100*NBResultLabels.count('positive')/len(NBResultLabels)) + "%")
-# else: 
-#     print("Overall Negative Sentiment")
-#     print("Negative Sentiment Percentage = " + str(100*NBResultLabels.count('negative')/len(NBResultLabels)) + "%")
-
+vectorizer = CountVectorizer(analyzer="word")
+freq_tweets = vectorizer.fit_transform(tweets)
+modelo = MultinomialNB()
+modelo.fit(freq_tweets,classes)
