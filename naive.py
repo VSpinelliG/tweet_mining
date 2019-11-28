0cc26edbc2bc9af1a58d8d605739bfa5fe12f4e9
vinicius <viinicius.sg@hotmail.com>
2019-11-28 15:26:47 +0000

    atualizando naive

--- naive.py
+++ naive.py
@@ -1,7 +1,6 @@
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.naive_bayes import BernoulliNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import BernoulliNB
 import csv
-import time
 
 def exibir_resultado(valor):
     frase, resultado = valor
@@ -37,11 +36,8 @@ def obter_dados_das_fontes():
 def tratamento_dos_dados(dados):
     dados_tratados = []
     for dado in dados:
-        print(dado)
         dado = dado.split('    ')
-        print(dado)
-        time.sleep(2)
-        
+        dados_tratados.append(dado)
 
     return dados_tratados
 
@@ -66,74 +62,74 @@ def pre_processamento():
     return dividir_dados_para_treino_e_validacao(dados_tratados)
 
 
-# def realizar_treinamento(registros_de_treino, vetorizador):
-#     treino_comentarios = [registro_treino[0] for registro_treino in registros_de_treino]
-#     treino_respostas = [registro_treino[1] for registro_treino in registros_de_treino]
+def realizar_treinamento(registros_de_treino, vetorizador):
+    treino_comentarios = [registro_treino[0] for registro_treino in registros_de_treino]
+    treino_respostas = [registro_treino[1] for registro_treino in registros_de_treino]
 
-#     treino_comentarios = vetorizador.fit_transform(treino_comentarios)
+    treino_comentarios = vetorizador.fit_transform(treino_comentarios)
 
-#     return BernoulliNB().fit(treino_comentarios, treino_respostas)
+    return BernoulliNB().fit(treino_comentarios, treino_respostas)
 
-# def realizar_avaliacao_simples(registros_para_avaliacao):
-#     avaliacao_comentarios = [registro_avaliacao[0] for registro_avaliacao in registros_para_avaliacao]
-#     avaliacao_respostas   = [registro_avaliacao[1] for registro_avaliacao in registros_para_avaliacao]
+def realizar_avaliacao_simples(registros_para_avaliacao):
+    avaliacao_comentarios = [registro_avaliacao[0] for registro_avaliacao in registros_para_avaliacao]
+    avaliacao_respostas   = [registro_avaliacao[1] for registro_avaliacao in registros_para_avaliacao]
 
-#     total = len(avaliacao_comentarios)
-#     acertos = 0
-#     for indice in range(0, total):
-#         resultado_analise = analisar_frase(classificador, vetorizador, avaliacao_comentarios[indice])
-#         frase, resultado = resultado_analise
-#         acertos += 1 if resultado[0] == avaliacao_respostas[indice] else 0
+    total = len(avaliacao_comentarios)
+    acertos = 0
+    for indice in range(0, total):
+        resultado_analise = analisar_frase(classificador, vetorizador, avaliacao_comentarios[indice])
+        frase, resultado = resultado_analise
+        acertos += 1 if resultado[0] == avaliacao_respostas[indice] else 0
 
-#     return acertos * 100 / total
+    return acertos * 100 / total
 
-# def realizar_avaliacao_completa(registros_para_avaliacao):
-#     avaliacao_comentarios = [registro_avaliacao[0] for registro_avaliacao in registros_para_avaliacao]
-#     avaliacao_respostas   = [registro_avaliacao[1] for registro_avaliacao in registros_para_avaliacao]
+def realizar_avaliacao_completa(registros_para_avaliacao):
+    avaliacao_comentarios = [registro_avaliacao[0] for registro_avaliacao in registros_para_avaliacao]
+    avaliacao_respostas   = [registro_avaliacao[1] for registro_avaliacao in registros_para_avaliacao]
 
-#     total = len(avaliacao_comentarios)
-#     verdadeiros_positivos = 0
-#     verdadeiros_negativos = 0
-#     falsos_positivos = 0
-#     falsos_negativos = 0
+    total = len(avaliacao_comentarios)
+    verdadeiros_positivos = 0
+    verdadeiros_negativos = 0
+    falsos_positivos = 0
+    falsos_negativos = 0
 
-#     for indice in range(0, total):
-#         resultado_analise = analisar_frase(classificador, vetorizador, avaliacao_comentarios[indice])
-#         frase, resultado = resultado_analise
-#         if resultado[0] == '0':
-#             verdadeiros_negativos += 1 if avaliacao_respostas[indice] == '0' else 0
-#             falsos_negativos += 1 if avaliacao_respostas[indice] != '0' else 0
-#         else:
-#             verdadeiros_positivos += 1 if avaliacao_respostas[indice] == '1' else 0
-#             falsos_positivos += 1 if avaliacao_respostas[indice] != '1' else 0
+    for indice in range(0, total):
+        resultado_analise = analisar_frase(classificador, vetorizador, avaliacao_comentarios[indice])
+        frase, resultado = resultado_analise
+        if resultado[0] == '0':
+            verdadeiros_negativos += 1 if avaliacao_respostas[indice] == '0' else 0
+            falsos_negativos += 1 if avaliacao_respostas[indice] != '0' else 0
+        else:
+            verdadeiros_positivos += 1 if avaliacao_respostas[indice] == '1' else 0
+            falsos_positivos += 1 if avaliacao_respostas[indice] != '1' else 0
 
-#     return ( verdadeiros_positivos * 100 / total, 
-#              verdadeiros_negativos * 100 / total,
-#              falsos_positivos * 100 / total,
-#              falsos_negativos * 100 / total
-#            )
+    return ( verdadeiros_positivos * 100 / total, 
+             verdadeiros_negativos * 100 / total,
+             falsos_positivos * 100 / total,
+             falsos_negativos * 100 / total
+           )
 
 registros_de_treino, registros_para_avaliacao = pre_processamento()
-# vetorizador = CountVectorizer(binary = 'true')
-# classificador = realizar_treinamento(registros_de_treino, vetorizador)
+vetorizador = CountVectorizer(binary = 'true')
+classificador = realizar_treinamento(registros_de_treino, vetorizador)
 
-# exibir_resultado( analisar_frase(classificador, vetorizador,"Esse é o melhor filme"))
-# exibir_resultado( analisar_frase(classificador, vetorizador,"this is the worst movie"))
-# exibir_resultado( analisar_frase(classificador, vetorizador,"awesome!"))
-# exibir_resultado( analisar_frase(classificador, vetorizador,"10/10"))
-# exibir_resultado( analisar_frase(classificador, vetorizador,"so bad"))
+exibir_resultado( analisar_frase(classificador, vetorizador,"bolsonaro deu show o resto é churumela e um palocci bem no meio da sua cara horrorosa"))
+exibir_resultado( analisar_frase(classificador, vetorizador,"bolsonaro realiza um discurso cheio de ódio lançando ataques aos seus antecessores perdeu grande oportunidade de construir pontes para o progresso"))
+exibir_resultado( analisar_frase(classificador, vetorizador,"eu nao aguento mais o bolsonaro que homem escroto meu deus"))
+exibir_resultado( analisar_frase(classificador, vetorizador,"bolsonaro foi bem"))
+exibir_resultado( analisar_frase(classificador, vetorizador,"estamos orgulhosos do presidente"))
 
-# percentual_acerto = realizar_avaliacao_simples(registros_para_avaliacao)
-# informacoes_analise = realizar_avaliacao_completa(registros_para_avaliacao)
+percentual_acerto = realizar_avaliacao_simples(registros_para_avaliacao)
+informacoes_analise = realizar_avaliacao_completa(registros_para_avaliacao)
 
-# verdadeiros_positivos,verdadeiros_negativos,falsos_positivos,falsos_negativos = informacoes_analise
+verdadeiros_positivos,verdadeiros_negativos,falsos_positivos,falsos_negativos = informacoes_analise
 
-# print("O modelo teve uma taxa de acerto de", percentual_acerto, "%")
+print("O modelo teve uma taxa de acerto de", percentual_acerto, "%")
 
-# print("Onde", verdadeiros_positivos, "% são verdadeiros positivos")
-# print("e", verdadeiros_negativos, "% são verdadeiros negativos")
+print("Onde", verdadeiros_positivos, "% são verdadeiros positivos")
+print("e", verdadeiros_negativos, "% são verdadeiros negativos")
 
-# print("e", falsos_positivos, "% são falsos positivos")
-# print("e", falsos_negativos, "% são falsos negativos")
+print("e", falsos_positivos, "% são falsos positivos")
+print("e", falsos_negativos, "% são falsos negativos")
 
 
