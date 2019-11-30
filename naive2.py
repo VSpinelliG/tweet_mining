0a63d01b85e89142e31ad3e0e3e82f08c6cded39
Vinicius Spinelli <viinicius.sg@hotmail.com>
2019-11-30 18:28:38 -0300

    :hammer:

--- naive2.py
+++ naive2.py
@@ -6,19 +6,17 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.model_selection import cross_val_predict
 
-dataset = pd.read_csv('tweets-mg.csv')
-dataset.count()
-
 dataset = pd.read_csv('c1.csv')
 tweets = []
 classes = []
 for text,sentiment in dataset.values:
-    tweets.append[text]
-    classes.append[sentiment]
+    tweets.append(text)
+    classes.append(sentiment)
 
 vectorizer = CountVectorizer(analyzer="word")
 freq_tweets = vectorizer.fit_transform(tweets)
 modelo = MultinomialNB()
+modelo.fit(freq_tweets,classes)
 
 testes = ['Esse governo está no início, vamos ver o que vai dar',
          'Estou muito feliz com o governo de Minas esse ano',
